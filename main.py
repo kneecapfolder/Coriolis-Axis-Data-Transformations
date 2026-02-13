545899f648ce42a28fdce8c2bd0d5ca0c4d2709c
Ilan Grodinsky <ilan7gr@gmail.com>
2026-02-13 17:14:06 +0200

    first commit. about to start parsing the data from tracker

---
+++ main.py
@@ -0,0 +1,23 @@
+import math
+
+r = .375 # 35.5cm
+
+def transform(x_prime, y_prime, theta):
+    # x' = R*cos(theta)
+    # y' = R*sin(theta)
+
+    R = math.hypot(x_prime, y_prime)
+
+
+    return x, y
+
+
+if __name__ == '__main__':
+    # w = 2*Pi/T
+    # T = 1.867 [s]
+
+    starting_angle = -61.8
+    w = 3.365 # omega
+
+    
+    
